88c77e229f0099cdbeb43e4649e7c8e0383d7219
Gadzilla143 <egor1231232@gmail.com>
2024-02-05 18:47:37 +0300

    Return Iframe communication

--- react/features/toolbox/components/web/Toolbox.tsx
+++ react/features/toolbox/components/web/Toolbox.tsx
@@ -374,7 +374,7 @@ const Toolbox = ({
             || overflowMenuButtons.some(({ key }) => key === 'reactions');
         const showRaiseHandInReactionsMenu = showReactionsInOverflowMenu && raiseHandInOverflowMenu;
 
-        // @TODO: return iframe communication const [isChatOpen, setIsChatOpen] = useState(false);
+        const [isChatOpen, setIsChatOpen] = useState(false);
 
         return (
             <div className = { containerClassName }>
@@ -394,9 +394,9 @@ const Toolbox = ({
                             height: 28,
                         }} />
                         <div onClick={() => {
-                            // @TODO: return iframe communication const event = new CustomEvent('changeChatStatus', { detail: !isChatOpen });
-                            // window.parent.document.dispatchEvent(event);
-                            // setIsChatOpen(!isChatOpen);
+                            const event = new CustomEvent('changeChatStatus', { detail: !isChatOpen });
+                            window.parent.document.dispatchEvent(event);
+                            setIsChatOpen(!isChatOpen);
                         }}
                             className = 'toolbox-icon'
                             style={{
